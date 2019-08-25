d5e14cd59fdb4a57b4e8710cc50d407a4a40c4a0
Jose Santiago <josesantiluna@gmail.com>
2019-08-25 13:21:12 -0400

    Creating Text field Restriction

--- grafica-satisfaccion/Controllers/MainViewController.swift
+++ grafica-satisfaccion/Controllers/MainViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class MainViewController: UITableViewController {
+class MainViewController: UITableViewController, UITextFieldDelegate {
+    
+    //MARK: Outlets
 
+    @IBOutlet weak var nameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameField.delegate = self
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.letters
+        let charSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: charSet)
     }
 }
