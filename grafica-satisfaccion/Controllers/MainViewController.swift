cd31d7e23d46ddeb307e10e760b8598a2289b184
Jose Santiago <josesantiluna@gmail.com>
2019-08-25 09:45:39 -0400

    Adding the basic structure to the project
    Adding ViewControllers :
    - MainViewController
    - ChartViewController
    To manage the main view and the view where the Charts are going to be
    
    Adding Models:
    - Question
    - ChartData
    
    Modifying the main storyboard respectably

---
+++ grafica-satisfaccion/Controllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  grafica-satisfaccion
+//
+//  Created by  Jose  Santiago on 8/25/19.
+//  Copyright © 2019  Jose  Santiago. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
